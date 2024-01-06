fcae3ef0d29cdf2f30c67755804906f393fafb50
vflei <vflei@Vladimir.attlocal.net>
2024-01-05 20:04:05 -0500

    Exercise 2 Test Demo

--- src/Demo/TestDemo.java
+++ src/Demo/TestDemo.java
@@ -6,7 +6,7 @@ public class TestDemo {
 	public static void main(String[]args) {
 		System.out.println(" Day 2 Git...");
 		System.out.println("Exercise 1");
-               
+		System.out.println("Exercise 2 Test Demo");       
 	}
 
 }
